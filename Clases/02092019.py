b95aecd9b10e52cc23a8eb1d6120063d5747aafb
Joakoxdhalo <joacojimeca@gmail.com>
2019-09-20 07:32:40 -0500

    * Add my chino Backup

---
+++ Clases/02092019.py
@@ -0,0 +1,1586 @@
+Lugares
+verbos
+
+diferencia entre chi y zài
+
+
+class Pronunciación del Chino mandarín vocales(a, o, e, i, u, ü):
+    def __init__():
+        return "siempre se cumple que las tíldes cumplen el orden descrito arriba"
+
+    def tonos():
+        Primer tono: Sacada de lengua
+        Segundo tono: Como si fuera pregunta
+        Tercer tono: En conver normal, se oye como plano y bajo
+        Cuarto tono: Como llamando a alguien
+
+    def suenan igual():
+        vocales = {a, i, u}
+        varias vocales = {ai, ia, iao, ua, ou, ei, ie}
+
+    def Suenan diferente:
+        o:
+            * Como la 'o' del español
+            * Como /uo/
+            * Como /uoa/
+        e:
+            * Persona moribunda pronunciando la 'e'
+            * Como /ea/
+        ü:
+            * Como la 'ü' de alemania ó como la 'u' de francia
+
+    def cambio natural(ui, iu):
+        * ui --> /uei/
+        Ejm:
+        shǔi --> /shuei/  agua
+
+        * iu --> /iou/
+        Ejm:
+        jiǔ --> /dBiou/  nueve
+
+    * un --> /u3n/
+    * ian --> /ien/
+    * sūn --> /su3(ē)n/  nieto
+    * yán --> /ién/  roca
+
+class pronunciación del chino mandarín consonantes():
+    def Iguales al español():
+    f - l - m - n
+
+    def Diferentes():
+        b --> 'p'
+        p --> 'p' inglesa, (Ej: peace)
+        d --> 't'
+        t --> 't' inglesa, (Ej: tea)
+        g --> 'k'
+        k --> 'k' inglesa, (Ej: kind, no noto la diff)
+        h --> 'j' (Ej: jarra)
+        z --> /dz/ (Ej: red zone)
+        c --> /ts/ (Ej: pizza)
+        s --> 's'
+        zi --> /dz/
+        ci --> /ts/
+        si --> 's'
+
+        De oreja a oreja y la lengua abajo:
+
+        j --> /ll/ (Ej: jeans)
+        q --> /ch/ (Ej: chino)
+        x --> /sh/ (Ej: she) un poco más suave que 'q', la punta de la lengua la pongo en el paladar
+
+        ju --> /d3ü/
+        qu --> /chü/
+        xu --> /shü/
+
+        Las siguientes  son con la lengua en el paladar y la boca abierta, con los dientes juntos:
+
+        zh --> /j/
+        ch --> /ch/
+        sh --> /sh/
+        r --> /ll/ (Ej: llamar), pero también tiende a ser una 'r'
+
+        y --> 'i' (Ej: yǒu /iou/ (amigo))
+        w --> 'u' (Ej: wà zi /uà dz/ (calcetines))
+
+
+class palabras difíciles o complejas():
+    amar
+    aì
+    爱
+
+    restaurante
+    cān tīng
+	餐厅
+
+    boca
+    zuǐ
+    嘴
+
+class días de la semana():
+    Lunes
+    xīng qī yī
+    星期一
+
+    Martes
+    xīng qī èr
+    星期二
+
+    Miércoles
+    xīng qī sān
+    星期三
+
+    Jueves
+    xīng qī sì
+    星期四
+
+    Viernes
+    xīng qī wǔ
+    星期五
+
+    Sábado
+    xīng qī liù
+    星期六
+
+    Domingo
+    xīng qī rì
+    星期日
+
+class números del 1 al 10():
+    Uno
+    yī
+    一
+
+    Dos
+    èr
+    二
+
+    Tres
+    sān
+    三
+
+    Cuatro
+    sì
+    /'s'/
+    四
+
+    Cinco
+    wǔ
+    五
+
+    Seis
+    liù
+    六
+
+    Siete
+    qī
+    七
+
+    Ocho
+    bā
+    八
+
+    Nueve
+    jiǔ
+    九
+
+    Diez
+    shí
+    十
+
+class palabras parecidas en escritura():
+    tú/usted
+    nǐ
+    你
+
+    Hola
+    nǐ hǎo
+    你好
+
+    él/ella
+    tā
+    她/他
+
+    también
+    yě
+    也
+
+    metro
+    dì tiě
+    地铁
+
+    amigo
+    péng you
+	朋友
+
+    amar
+    aì
+    爱
+
+	Partícula para pregunta cerrada: No significa nada
+    ma
+    吗
+
+    mamá
+    māmā
+    妈妈
+class familiares():
+    mamá
+    māmā
+    妈妈
+
+    papá
+    bàba
+    爸爸
+
+    hermano mayor
+    gēgē
+    哥哥
+
+    hermano menor
+    dìdì
+    弟弟
+
+    hermana mayor
+    jiě jiě
+    姐姐
+
+    hermana menor
+    mèi mei
+    妹妹
+
+    amigo
+    péng you
+    朋友
+
+class palabras importantes():
+	comer
+	chī
+	吃
+
+	comida
+	cài
+	菜
+
+class partículas():
+    Partícula de pertenencia
+    de
+    的
+
+    partícula "también"
+    yě
+    也
+
+    Partícula men: Plural
+    men
+    们
+
+    Partícula "Y"
+    hé
+    和
+
+    Partícula para pregunta cerrada: No significa nada
+    ma
+    吗
+
+    Partícula dónde
+    nǎ
+    哪
+
+    Partícula "qué"
+    shen me
+    什么
+
+    Partícula "muy": antes del verbo o adjetivo
+    hěn
+    很
+
+    Sí o verbo ser
+    shì
+    是
+
+class medios de transporte():
+    avión
+    fēi jī
+    飞机
+
+    metro
+    dì tiě
+    地铁
+
+    bus
+    qì chē
+    汽车
+
+    tren
+    huǒ chē
+    火车
+
+    taxi
+    chū zū chē
+    出租车
+
+    Carro
+    chē
+    车
+
+    abrir carro
+    kāi chē
+    开车
+
+	Estación de metro
+	ditie zhan
+	地铁站
+
+	Estación de bus
+	qi che zhan
+	汽车站
+
+	Aeropuerto
+	fēi jī chǎng
+	飞机场
+
+class partes del cuerpo():
+    cabeza
+    tóu
+    头
+
+    hombro
+    jiān bǎng
+    肩膀
+
+    rodillas
+    xī gài
+    膝盖
+
+    pie
+    jiǎo
+    脚
+
+    ojos
+    yǎn jīng
+    眼睛
+
+    oreja
+    ěr duǒ
+    耳朵
+
+    boca
+    zuǐ
+    嘴
+
+
+class deportes con bola():
+	patear
+	tī
+	踢
+
+    Fútbol
+    zú qiú
+    足球
+
+    Baloncesto
+    lán qiú
+    篮球
+
+    Tenis
+    wǎng qiú
+    网球
+
+    Bádminton
+    yǔ máo qiú
+    羽毛球
+
+    Béisbol
+    bàng qiú
+    棒球
+
+    Balón mano
+    shǒu qiú
+    手球
+
+    Hockey
+    qǔ gùn qiú
+    曲棍球
+
+    Rugby
+    gǎn lǎn qiú
+    橄榄球
+
+    Voleibol
+    pái qiú
+    排球
+
+    ping pong
+    pīng pāng qiú
+    乒乓球
+
+class deportes otros():
+    Boxeo
+    quán jī
+    拳击
+
+    Judo
+    róu dào
+    柔道
+
+    Lucha
+    shuāi jiāo
+    摔跤
+
+    Ajedrez chino
+    xiàn qí
+    象棋
+
+    Atletismo
+    pǎo
+    跑
+
+    Ciclismo
+    zì xíng chē
+    自行车
+
+    Natación
+    yóu yǒng
+    游泳
+
+    Olimpiadas
+    ào lín pǐ kè
+    奥林匹克
+
+    Juegos olímpicos
+    ào lín pǐ kè bǐ sài
+    奥林匹克比赛
+
+
+Yo te amo
+/yo amar tú/
+wǒ ài nǐ
+我爱你
+
+el ella
+tā tā
+他  她
+
+Profe
+lǎo shī
+老师
+
+Mierda
+shǐ
+屎
+
+Gracias
+xiè xiè
+谢谢
+
+libro
+shū
+书
+
+Lo siento
+duì bù qǐ
+对不起 ---> Como aparece en el traductor
+对比起 ---> Como lo anoté en clase
+
+yo te amo mucho
+\yo muy amar tú\
+wǒ hěn ài nǐ
+我很爱你
+
+Mi mamá
+wǒ de māmā
+我的妈妈
+
+Mi libro
+wǒ de shū
+我的书
+
+tu libro
+nǐ de shū
+你的书
+
+Yo te amo también
+/Yo también amo a tí/
+wǒ yě aì nǐ
+我也爱你
+
+ellas
+tā men
+她们
+
+ellos
+tā men
+他们
+
+vosotros
+nǐ men
+你们
+
+nosotros
+wǒ men
+我们
+
+Colombia
+gē lún bǐ yǎ
+哥伦比亚
+
+Colombiano
+gē lún bǐ yǎ rén
+哥伦比亚人
+
+Yo soy colombiano
+wǒ shì gē lún bǐ yǎ rén
+我是哥伦比亚人
+
+Ella también es colombiana
+tā yě shì gē lún bǐ yǎ rén
+他也是哥伦比亚人
+
+mi mamá también es colombiana
+wǒ de māmā yě shì gē lún bǐ yǎ rén
+我的妈妈也是哥伦比亚人
+
+**********************************************************************
+
+Feliz
+kuài lè
+快乐
+
+Hoy estoy muy feliz
+Jīn tiān wǒ hěn kuài lè
+今天我很快乐
+
+Feliz cumpleaños te deseo a tí
+zhù nǐ shēng rì kuài lè
+祝你生日快乐
+
+agua
+shuǐ
+水
+
+gustar
+xǐ huān
+喜欢
+
+Te invito a comer comida china
+Wǒ qǐng nǐ chī zhōngguó cài
+我请你吃中国菜
+
+Me invitas a tomar café colombiano.
+Nǐ qǐng wǒ hē gēlúnbǐyǎ kāfēi
+你请我喝哥伦比亚咖啡
+
+poder
+nang
+能
+
+Yo no puedo
+wǒ bu neng
+我不能
+
+Tu puedes manejar?
+ni neng keiche ma?
+你能开车吗？
+
+Tú no sabes quién soy yo
+\Tu no sabes yo soy quién?\
+ni bu zhidao wǒ shi shei?
+你不知道我是谁？
+
+Ella es mi amiga?
+ta shi wǒ de pengyou
+她是我的朋友
+
+Hombre
+nán
+男
+
+Mujer / Hembra
+nǔ
+女
+
+Novia
+
+Por favor siéntese:
+qǐng zuò
+请坐
+
+comer
+chi
+吃
+
+tomar
+hē
+喝
+
+Yo te invito a tí
+wǒ yao qing ni
+我邀请你
+
+
+Yo te invito a comer comida china
+wǒ yao qing ni chi zhong guo cai
+我邀请你吃中国菜
+
+no
+bu
+不
+
+Los colombianos no comen comida de perro
+
+Profe de alemán
+de guo lǎo shī
+德国老师
+
+hablar ---> El profesor sólo pronuncia el shuo
+shuō hua
+说话
+
+Yo hablo  --> Dudas
+wǒ shuo hua
+我说话
+
+
+Yo tengo
+wǒ io
+我有
+
+Yo no tengo
+我没有
+
+NUNCA SE DICE "BU" para decir "no hay" o "no tengo" SE DICE MEIYOU
+
+dinero
+qian
+钱
+
+No tengo dinero
+wǒ meiyou qian
+我没有钱
+
+No tengo dinero
+wǒ meiyou shu
+No tengo libro
+
+Me gusta tomar avión
+wǒ xihuan zuo feiji
+我喜欢坐飞机
+
+Tú eres Colombiano?
+Nǐ shì gēlúnbǐyǎ rén ma?
+你是哥伦比亚人吗
+
+Ella es de china
+ta shi zhong guo
+
+Usted es de china?
+ni shi zhong guo ren ma?
+你是中国人吗
+
+Yes
+shi
+是
+
+No
+bu
+不
+
+De nada
+
+Por favor
+
+Sentare
+
+francia
+fa guo
+法国
+
+inglaterra
+ying guo
+英国
+
+alemania
+de guo
+德国
+
+Estados unidos
+mei guo
+美国
+
+Rusia
+e guo
+俄国
+
+**************************************************
+
+Dónde está el restaurante chino?
+Zhung wǒ can guan zai nali
+/China restaurante donde/
+中国 餐厅 在哪？
+
+ir
+qu
+去
+
+Donde esta tu mama
+ni da mama sena
+
+A dónde vas?
+ni qu na?
+你去哪?
+
+Dónde estás?
+ni zai na?
+你在哪?
+
+Dónde vives tú?
+ni zhu na?
+你住哪
+
+Yo vivo en Bosa:
+Wo zhu Bosa
+握住 Bosa
+
+Pensar, echar de menos.
+xiang
+想
+
+Yo te extraño
+Wo xiang ni
+
+Yo quiero vivir en china
+Wo xiang zhu zhong guo
+我想住中国
+
+pelicula
+dian ying
+电影
+
+ver
+kan
+看
+
+Me gusta ver la película en chino
+wǒ xihuan kan dian ying
+我喜欢看电影
+
+jugar  (Para los deportes con la mano)
+da
+打
+
+Jugar baloncesto
+da lan qui
+打蓝驱
+
+jugar Voleibol
+da pai qiu
+打排球
+
+Jugar tenis
+da wang qiu
+打网球
+
+Jugar ping pong
+da ping pong qiu
+打乒乓球
+
+Jugar ---> Con la pierna
+ti zu qiu
+踢足球
+
+Te gusta jugar ping pong?
+ni xing uan ping pong? (INCOMpleta)
+
+Yo soy profesor
+wǒ shi laoshi
+我是老师
+
+Universidad del rosario
+Rosario da que
+Rosario 大学
+
+Universidad Nacional
+guo li da xue
+国立大学
+
+
+Estudiar
+xuexi
+学习
+
+Preposición 'en'
+zai
+在
+
+Yo estudio en la nacho
+\Yo en la nacho estudio chino\
+wǒ zai guo li da xue
+我在国立大学
+
+Yo estudio francés en francia
+wǒ zai fa guo xuexi francés
+我在法国学
+
+*** Yo estudio portugués en brasil  -----> INCOMPLETO
+\yo estudio en brasil portugués\
+wǒ zai ba xi xuexi puta oya
+
+
+Chino
+haniu
+
+cctv.com
+
+Bonita
+piao liang
+漂亮
+
+Handsome boy
+shuai
+帅
+
+ocupado
+mang
+忙
+
+Yo estoy muy ocupado
+wǒ hěn máng
+我很忙
+
+No estoy ocupado
+wǒ bu mang
+我不忙
+
+Ropa
+yī fú
+衣服
+
+costoso
+áng guì
+昂贵
+
+barato
+lián jià
+廉价
+
+En china la ropa es muy cara, en Colombia la ropa es muy barata.
+/la ropa muy cara en china, la ropa muy barata en Colombia/
+
+
+/la ropa muy barata en China, la ropa muy cara en Colombia./
+yī fú hěn lián jià zhōng guó, yī fú hěn áng guì gēlúnbǐyǎ.
+衣服很廉价中国， 衣服很昂贵哥伦比亚
+
+
+2) alto 
+3) netro
+4)
+5)
+
+
+
+Lo de la hoja:
+Cerdo
+pollo
+pescado
+res
+
+restaurante
+
+1____________________________________________________________________________________
+
+
+
+Por favor siéntese
+qing zuo
+请坐
+
+gracias
+xie xie
+谢谢
+
+por favor entrar
+qing jing
+
+Hermana mayor
+
+Hermana menor
+
+papa
+ba ba
+爸爸
+
+
+mama
+ma ma
+妈妈
+
+
+
+Colombia
+ge lun bi ya
+哥伦比亚
+
+
+Yo soy Colombiano
+wǒ shi ge lun bi ya ren
+我是哥伦比亚人
+
+Partícula de pregunta cerrada: ma
+吗
+
+Usted es Chino?
+ni shi zhong guo ren ma?
+你是中国人吗
+
+De qué país eres tú?
+Ni shi na guo ren?
+你是哪国人?
+
+Partícula dónde: nǎ
+哪
+
+De qué país es Felipe?
+Felipe shi nǎ guo ren?
+Felipe 是哪国人
+
+Verbo Saber
+zhi dao
+知道
+
+Tú sabes?
+ni zhi dao ma?
+你知道吗?
+
+partícula y: hé
+和
+
+Mi mamá es colombiana
+
+
+Mi papá también es colombiano
+
+llamarse
+jiào
+叫
+
+cómo te llamas?
+/Tu llamas qué/
+nǐ jiào shén me?
+你叫什么
+
+Me llamo Luis
+wǒ jiao Luis
+我叫Luis
+
+yo no sé:
+wǒ bù zhī dào
+我不知道
+
+Cómo se llama su perro?
+/Tu perro llama qué/
+ni de gou jiao shen me?
+你打狗叫什么
+
+Su perro se llama Boris
+ta de gou jiao Boris
+他的狗叫Boris
+
+adiós
+zài jiàn
+再见
+
+2******************************************************************************************
+11092019
+
+Celular
+Shǒu jī
+手机
+
+
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+Semana
+Domingo
+anteayer
+
+
+
+    izquierda
+    zuǒ
+    左
+
+    Adelante
+    qián
+    钱
+
+    Atrás
+    hòu
+    后
+
+
+class comidas():
+	__init__():
+		return "Los verbos de estos sustantivos se sacan poniéndo el verbo cài antepuesto"
+
+    desayuno
+    zǎo cān
+    早餐
+
+    almuerzo
+    wǔ cān
+    午餐
+
+    cena
+    wǎn cān
+    晚餐
+
+Hay (de tener)
+yǒu
+有 ----> Aparce diferente al prof
+
+
+No hay
+méi yōu
+没有
+
+Hermana menor
+mèi mei
+妹妹
+
+Pasado mañana es viernes
+
+class temporalidades():
+
+    En la mañana 5am 8am
+    zǎo shang
+    早上
+
+    En la mañana 8am 12pm
+    shàng wǔ
+    上午
+
+    medio día
+    zhōng wǔ
+    中午
+
+    En la tarde
+    xià wǔ
+    下午
+
+    En la noche
+    wǎn shang
+    晚上
+
+class temporalidades_2():
+    Año
+    nián
+    年
+
+    mes
+    yuè
+    月
+
+    Año nuevo
+    xīn de yī nián
+    新的一年
+
+    navidad
+    shèng dàn jié
+    圣诞节
+
+class temporalidades_3():
+    Anteayer
+    qián tiān
+    前天
+
+    Ayer
+    zuō tiān
+    昨天
+
+    hoy
+    Jīn tiān
+    今天
+
+    mañana
+    míng tiān
+    明天
+
+    Ante ayer va primero que hoy, por eso se escribe así en chino
+
+    pasado mañana
+    hòu tiān
+    后天
+
+class direcciones():
+    Arriba
+    shàng
+    上
+
+    abajo
+    xià
+    下
+
+    derecha
+    yòu
+    右
+
+
+Extrañar/ echar de menos
+xiǎng
+想
+
+Yo quiero estudiar francés
+
+No quiero estudiar 
+wǒ bu xiang ...
+
+Ir
+
+Yo quiero ir a San Andrés (B)
+Wo xiang qù San Andrés
+
+Escibir
+xie
+
+Hoy en la noche quiero comer comida china
+jīn tiān wǎn shang wǒ xiǎng chī zhōng guo cài
+今天晚上我想吃中国菜
+
+Por favor siéntese
+qǐng zuò
+请坐
+
+Silencio
+ān jìng
+安静
+
+1)poder
+néng
+能
+
+Yo puedo escribir los símbolos chinos
+
+Yo puedo digitar
+
+Yo puedo hablar
+
+Tu puedes aprender inglés?
+
+2)Gustar
+xǐ huān
+喜欢
+
+
+Con 'xihuan':
+Me gusta jugar ping pong
+
+A los colombianos les gusta jugar futbol
+(ti) para "patear"
+
+3)Tener
+yǒu
+有
+
+4)ocupado
+mā
+忙
+
+Estás ocupado?
+ni ma me
+
+5)Difícil
+nán
+难
+
+
+El ejercicio es hacer oraciones largas con esos 5 verbos:
+
+1. Mi papá puede viajar en avión
+
+
+wǒ ta bàba néng qù lǔxing fei ji
+我的爸爸能去撸型飞机
+sinónimos de viajar:
+游
+
+路行
+
+
+
+2. A mi mamá le gustan las manzanas
+wǒ de màma xǐ huān Píngguǒ
+我的妈妈喜欢苹果
+
+3. Ellos también tienen frutas
+ta men yě yǒu shuǐguǒ
+他们也有水果
+
+4. El profesor está ocupado escribiendo
+lǎoshī máng xiě
+老师忙写
+
+
+fácil.
+
+
+
+
+Segundo ejercicio organizar:
+
+1) Cómo se llama usted?
+你 叫 什么?
+nǐ jiào shén me?
+
+2) De qué nacionalidad es ella?
+她是哪国人?
+tā shì nǎ guórén
+
+3) Yo amo a mi mamá
+wǒ ài wǒ ta mama
+我爱我她妈妈
+
+4) Dónde está el baño?
+cèsuǒ zài nǎ
+厕所在哪
+
+5) Te deseo feliz cumpleaños
+祝你 生日 快乐 祝你
+zhù nǐ shēng rì kuài lè
+
+*******************************************************
+16092019
+
+café
+kā fēi
+咖啡
+
+té
+chá
+茶
+
+agua
+shuǐ
+水
+
+Yo tomo café chino
+wǒ hē zhōng guó kā fēi
+我喝中国咖啡
+
+De qué país eres tú?
+nǐ shì nǎ guó rén
+你是哪国人
+
+comer
+chī
+吃
+
+comida
+cài
+菜
+
+Comida china
+zhōng guó cài
+
+Tú comes comida china
+nǐ chī zhōng guó cài ma?
+你吃中国菜吗?
+
+Yo como comida francesa
+wǒ chī fà guó cài
+我吃法国菜
+
+class carnes():
+
+    Carne de res
+    niú ròu
+    牛肉
+
+    Carne de Cerdo
+    zhū ròu
+    猪肉
+
+    Carne de pollo
+    jī ròu
+    鸡肉
+
+    Carne de pescado
+    yú ròu
+    鱼肉
+
+Yo no como carne de perro
+wǒ bù chī gǒu ròu
+我不吃狗肉
+
+Qué come usted?
+nǐ chī shén me?
+你吃什么?
+
+zài:
+- estar
+- en
+- estar en
+在
+
+dónde está?
+zài nǎ
+在哪？
+
+Dónde está el baño?
+/El baño está dónde?/
+cè suǒ zài nǎ?
+厕所在哪
+
+Dónde está el baño de caballeros?
+nán cè suǒ zài nǎ
+男厕所在哪
+
+banco
+yín háng
+银行
+
+Dónde está el banco de china?
+/China banco está dónde?/
+zhōng guó yín háng zài nǎ?
+
+Dónde está el banco de Bogotá?
+/Bogotá banco está dónde?/
+Bogotá yín háng zài nǎ?
+
+Dónde está el banco de Colombia?
+/Colombia banco está dónde?/
+gē lún bǐ yǎ yín háng zài nǎ?
+
+restaurante
+cān tīng
+餐厅
+
+Dónde está el restaurante francés?
+fa guó cān tīng zài nǎ?
+
+tienda
+shāng dìan
+商店
+
+Super - mercado
+chāo shì
+超市
+
+Dónde está el supermercado?
+chāo shì zài nǎ?
+
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+Amigo ----> creo
+péng you
+朋友
+
+Ahora
+xiàn zài
+现在
+
+Qué hora es?
+/ahora qué hora?/
+xiàn zài jǐ diǎn?
+现在几点
+
+minuto
+fēn
+分
+
+las ocho y media
+8 diǎn bàn
+8 点 半
+ó se puede:
+8 点 30 fēn
+
+las ocho y cuarto
+8 diǎn kè
+8 点 1 刻
+
+Faltan cinco para las nueve
+chà 5 fēn 9 diǎn
+差 5 分 9 点
+
+
+Yo estudio chino en Colombia
+wǒ sai de guo shueshi 
+
+Yo estudio chino en la nacho
+wǒ hai niu xuexi guo li da xue
+wo zai ge lun bi ya guo li da xue ...
+我在哥伦比亚国立大学。。。
+
+    Universidad Nacional
+    guo li da xue
+    国立大学
+
+cada
+měi
+没
+
+Yo me levanto cada día a las 5
+/yo cada día por la mañana a las 5 me levanto/
+wǒ měi tiān zǎo shang 5 diǎn qǐ chuáng
+我每天早上 5 点起床
+
+Yo cada día me duermo a las 11 de la noche
+wǒ měi tiān wǎn shang 11 shuì jiào Shíyī   ---> Verificar
+
+tarea
+zuò yè
+作业
+
+Hacer la tarea
+zuò zuò yè
+做作业
+
+levantarse
+qǐ chuáng
+起床
+
+dormir
+shuì jiào
+睡觉
+
+Yo me duermo todos los días a las 8pm
+/Yo todos los días por la noche a las 8pm me duermo/
+wǒ měi tiān wǎn shang bā(8) shaì jiào
+我每天晚上八睡觉
+
+desde ... hasta
+cóng ... dào
+从 。。。 到
+
+clase
+kè
+课
+
+clase de chino
+hàn yū kè
+汉语课
+
+v(falta verificar)
+Todas las noches yo duermo desde las 10pm hasta las 6am
+/yo todos los días por la noche duermo desde las 10 hasta las 6 de la mañana/
+wǒ měi tiān wǎn shang shuì jiào cóng 10 dào 6 zǎo shang
+
+Casa ---> Sin verificar
+fàng zi
+房子
+
+yo tengo clase de chino a las 5pm 
+wo 
+
+El lunes y el Miércoles a las 5 de la tarde tengo clase de chino
+xīng qī yī hé xīng qī sān wǒ méi yōu hàn yū kè
+星期一  和  星期三  我没有汉语课
+
+
+
+**** Me genera la duda el xià
+Quiero jugar al fútbol hoy
+Jīntiān xià wǒ xiǎng tī zúqiú
+今天     下  我  想   踢  足球
+
+Yo estoy aprendiendo chino en china.
+Wǒ zài zhōngguó xuéxí hànyǔ
+我在中国学习汉语
+
+
+**********************************************************************************
+18092019
+Hablar
+shǔo
+
+
+YO hablo chino
+wǒ shuō hàn yǔ
+我说汉语
+
+Idioma inglés
+yīng yǔ
+英语
+
+Idioma francés
+fǎ yǔ
+法语
+
+Idioma chino
+hàn yǔ
+汉语
+
+España
+xī bān yán
+西班牙
+
+Idioma español
+xī bān yá yǔ
+西班牙语
+
+Idioma Alemán
+dé yǔ
+德语
+
+Tu hablas inglés?
+nǐ shuō yīng yǔ ma?
+你说英语吗?
+
+Yo hablo español inglés y chino?
+wǒ shuō xī bān yán yǔ, yīng yǔ hé hàn yǔ
+我说西班牙语，英语 和 汉语
+
+A los colombianos les gusta tomar café
+gē lún bǐ yǎ rén ài hē kāfē
+哥伦比亚人爱喝咖啡
+
+A los colombianos les gusta la comida china
+gē lún bǐ yǎ rén ài chī zhōng guó cài
+哥伦比亚人爱吃中国菜
+
+Yo soy tu amigo
+
+
+Tu y yo somos amigos
+
+China y Colombia son amigos
+zhōng guó hé gēlúnbǐyǎ shi péng you
+中国和哥伦比亚是朋友
+
+frío
+lèng
+冷
+
+Estoy muy ocupado
+wǒ hěn máng
+我很忙 ----> Sin comprobar
+
+verbo visitar
+Fǎngwèn
+访问
+
+
+1) verbo debería
+yīng gāi
+应该
+
+
+verbo viajar
+desayuno
+
+2) verbo poder
+néng
+能
+
+3) Invitar/ Gtraductor: por favor
+Qǐng
+请
+
+4) Ocupado
+máng
+忙
+
+5) caro
+guì
+贵
+
+6) difícil
+nán
+难
+
+1) Yo debería visitar la mamá de mi amigo
+wǒ yīng gāi Fǎngwèn péng you de māma
+我  应该  访问 朋友 的妈妈
+
+2) Yo puedo escribir carácteres chinos
+/yo puedo escribir chinos carácteres/
+wǒ néng xiě hán yǔ xìn
+我能写信汉语
+
+3)Yo te invito
+wǒ qǐng nǐ
+我请你
+4)...
+5)...
+6)
+
+
