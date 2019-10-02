53db90a377838d44523af625da9ae737371e229d
Joakoxdhalo <joacojimeca@gmail.com>
2019-10-02 17:46:57 -0500

    * Hacer correción del parcial 1 de chino 2

--- Clases/Parcial_1_de_chino2.py
+++ Clases/Parcial_1_de_chino2.py
@@ -1,8 +1,8 @@
 Parcial de chino 2:
 
-1) 晚餐		2) --	3) 午餐		4) --			5) 汉子
-wǎn cān				wū cān						han zì
-comida				almuerzo	Universidad		símbolo chino
+1) 晚餐		2) 作业		3) 午餐		4) 大学			5) 汉子			6)学习
+wǎn cān		zuò yè		wǔ cān		dà xué				han zì		Estudiar
+comida		tarea		almuerzo	Universidad		símbolo chino	xuéxí
 
 
 aeropuerto
@@ -10,45 +10,45 @@ feī jī chāng
 飞机场
 
 película
---
---
+diàn yǐng
+电影
 
 metro
---
---
+dì tiě
+地铁
 
 universidad
---
---
+dà xué
+大学
 
 ropa
---
---
+yī fú
+衣服
 
 fútbol
---
---
+zú qiú
+足球
 
 tenis
---
---
+wǎng qiú
+网球
 
 ping pong
 pīng pāng
 乒乓
 
 viajar
---
---
+lǚ you
+旅游
 
 fácil
---
---
+jiǎn dān
+简单
 
 2) Ordenar las oraciones
 
 Cómo se llama tu mamá
-nǐ de māma jìao shen me?
+nǐ de māma jiào shén me?
 你的妈妈叫什么?
 
 De dónde es tu hermano mayor?
@@ -60,53 +60,61 @@ zhōng guó cān tīng zài nǎ?
 中国餐厅在哪?
 
 Tú y yo somos amigos
-nǐ hé wǒ shì pēng you
+nǐ hé wǒ shì péng you
 你和我是朋友
 
 Yo no como comida china
-wǒ bú chī zhōng guó cài
+wǒ bù chī zhōng guó cài
 我不吃中国菜
 
 4) Poner fonética y el tono sobre cada oración y traducir al español
 
 Me gusta viajar
-我喜欢viajar
-wǒ xi huān ...
+我喜欢旅游
+wǒ xǐ huān lǚ you
 
 Dónde está el aeropuerto?
-feī jī chāng zài nǎ?
+feī jī chǎng zài nǎ?
 飞机场在哪?
 
 Yo quiero ir a china
-wǒ xiang qu zhōng guó
+wǒ xiǎng qù zhōng guó
 我想去中国
 
 Mi mamá todos los días por la mañana a las 5 se despierta para hacer el desayuno
-wǒ de māma mèi tiàn ... ... wǔ diàn ... ... zu ... cān
-我的妈妈每天 。。。 。。。 五点 。。。 。。。 。。。 餐
+wǒ de māma mèi tiàn zǎo shang wǔ diàn qǐ chuáng zuò zǎo cān
+我的妈妈每天早上五点起床昨早餐
 
 tú todos los días por la noche a las 11 te acuestas.
-tā mēi tiàn won ... shi yi diàn ... acostarse
-他每天晚 。。。 十一 点 。。。。。。
-
+tā měi tiān wǎn shang shí yī diǎn shuì jiào
+他每天晚上十一点睡觉
 
 5) Poner el tono sobre cada palabra:
-hē		hé		jìao		nǎ		chī
+hē		hé		jiào		nǎ		chī
 喝		和		叫			那		吃
 beber	y 		llamarse	Dónde 	comer
 
 6) Construir 5 oraciones con cada palabra:
-mang	neng	...		...			...
-忙		能		...		...			...
-Ocupado	Poder	...		difícil		...
-
+máng	néng	guì		nán			yīng gāi
+忙		能		贵		难			应该
+Ocupado	Poder	caro	difícil		Deber
 
 Yo estoy muy ocupado
 wǒ shì hen mang
 我是很忙
 
 Yo puedo hacer mi tarea
-wǒ neng zuo zuo yi
-我能 。。。
+wǒ néng zuó zuò yè
+我能做作业
 
-。。。
+La ropa en Colombia es muy cara, la ropa en china es muy barata
+gē lún bǐ yǎ yī fú shì áng guì, zhōng guó yī fú shì lián jià
+哥伦比亚衣服是昂贵， 中国衣服是廉价
+
+Qué es difícil?
+nán shì shén me?
+男是什么?
+
+Debo saberme 1000 símbolos chinos
+wǒ yīng gāi zhī dào qiān hàn zì
+我应该知道千汉字
