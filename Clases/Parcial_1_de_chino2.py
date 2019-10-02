ec8f8e43efd5e1c7980fb4cf90977276c348e42c
Joakoxdhalo <joacojimeca@gmail.com>
2019-10-02 15:58:02 -0500

    * Añadir el parcial de chino 2 hasta donde más lo pude resolver

---
+++ Clases/Parcial_1_de_chino2.py
@@ -0,0 +1,112 @@
+Parcial de chino 2:
+
+1) 晚餐		2) --	3) 午餐		4) --			5) 汉子
+wǎn cān				wū cān						han zì
+comida				almuerzo	Universidad		símbolo chino
+
+
+aeropuerto
+feī jī chāng
+飞机场
+
+película
+--
+--
+
+metro
+--
+--
+
+universidad
+--
+--
+
+ropa
+--
+--
+
+fútbol
+--
+--
+
+tenis
+--
+--
+
+ping pong
+pīng pāng
+乒乓
+
+viajar
+--
+--
+
+fácil
+--
+--
+
+2) Ordenar las oraciones
+
+Cómo se llama tu mamá
+nǐ de māma jìao shen me?
+你的妈妈叫什么?
+
+De dónde es tu hermano mayor?
+nǐ de gēgē zài nǎ guó rén?
+你的哥哥是哪国人?
+
+Dónde está el restaurante chino
+zhōng guó cān tīng zài nǎ?
+中国餐厅在哪?
+
+Tú y yo somos amigos
+nǐ hé wǒ shì pēng you
+你和我是朋友
+
+Yo no como comida china
+wǒ bú chī zhōng guó cài
+我不吃中国菜
+
+4) Poner fonética y el tono sobre cada oración y traducir al español
+
+Me gusta viajar
+我喜欢viajar
+wǒ xi huān ...
+
+Dónde está el aeropuerto?
+feī jī chāng zài nǎ?
+飞机场在哪?
+
+Yo quiero ir a china
+wǒ xiang qu zhōng guó
+我想去中国
+
+Mi mamá todos los días por la mañana a las 5 se despierta para hacer el desayuno
+wǒ de māma mèi tiàn ... ... wǔ diàn ... ... zu ... cān
+我的妈妈每天 。。。 。。。 五点 。。。 。。。 。。。 餐
+
+tú todos los días por la noche a las 11 te acuestas.
+tā mēi tiàn won ... shi yi diàn ... acostarse
+他每天晚 。。。 十一 点 。。。。。。
+
+
+5) Poner el tono sobre cada palabra:
+hē		hé		jìao		nǎ		chī
+喝		和		叫			那		吃
+beber	y 		llamarse	Dónde 	comer
+
+6) Construir 5 oraciones con cada palabra:
+mang	neng	...		...			...
+忙		能		...		...			...
+Ocupado	Poder	...		difícil		...
+
+
+Yo estoy muy ocupado
+wǒ shì hen mang
+我是很忙
+
+Yo puedo hacer mi tarea
+wǒ neng zuo zuo yi
+我能 。。。
+
+。。。
