a23c9e8d1a3d2240fabf68019847ad82a983c1ef
Joakoxdhalo <joacojimeca@gmail.com>
2019-10-21 12:53:32 -0500

    * Add 5 frases de chino to review

---
+++ Clases/Repasode5frasesChino1.py
@@ -0,0 +1,19 @@
+1) Hoy estoy muy ocpado, yo no tengo tiempo
+jīn tiān wǒ hěn máng, wǒ méi yǒu shí jiān
+今天我很忙，我没有时间
+
+2) xīng qī tiān wǒ xī huān qí zì xíng chē
+El domingo me gusta montar en bicicleta
+星期天我喜欢骑自行车
+
+3) hablar chino es muy fácil, los símbolos chinos son muy difíciles
+shuō hàn yǔ hěn jiǎn dān, xiě hàn zì hěn nán
+说汉语很简单，写汉字很难
+
+４) Yo estudio chino en Beijing
+wǒ zài zhōng guó běi jīng xué xí hàn yǔ
+我在中国北京学习汉语
+
+5) A los colombianos les gusta jugar fútbol, a los chinos les gusta jugar ping pong
+gē lún bǐ yà rén xī huān tī zú qiú, zhōng guó rén xī huān dǎ pīng pāng qiú
+哥伦比亚人喜欢踢足球，中国人喜欢打乒乓球
