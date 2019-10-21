cc656ddc95c4b7f135e07a83b71a838d64028a12
Joakoxdhalo <joacojimeca@gmail.com>
2019-10-21 14:26:06 -0500

    * Add Repasode5frasesChino0.py

---
+++ Clases/Repasode5frasesChino0.py
@@ -0,0 +1,19 @@
+1) Yo te invito a comer comida china
+wǒ qǐng nǐ chī zhōng guó cài
+我请你吃中国菜
+
+2) Dónde está el restaurante chino?
+fà guó cān tīng zài nǎ?
+法国餐厅在哪
+
+3) Yo hablo español y chino
+wǒ shuō xī bān yá yǔ hé hàn yǔ
+我说西班牙语和汉语
+
+4) Tú y yo somos amigos
+nǐ hé wǒ shì péng yǒu
+你和我是朋友
+
+5) Los colombianos no comen comida china
+gē lún bǐ yǎ rén bù chī gou ròu
+哥伦比亚人不吃狗肉
