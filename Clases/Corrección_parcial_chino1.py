30c5e1b802b1dfd6bb2a5f5f8eaf78b71f6f0fcf
Joakoxdhalo <joacojimeca@gmail.com>
2019-10-21 12:55:00 -0500

    * Add corrección de parcial de chino 1

---
+++ Clases/Corrección_parcial_chino1.py
@@ -0,0 +1,55 @@
+Carne de res
+niú ròu
+牛肉
+
+Carne de pollo
+jī ròu
+鸡肉
+
+Carne de cerdo
+zhū ròu
+猪肉
+
+mamá
+māmā
+妈妈
+
+amigo
+péng yǒu
+朋友
+
+Ser o estar
+shì
+是
+
+No
+bù
+不
+
+té
+chá
+茶
+
+Español
+xī bān yá yǔ
+西班牙语
+
+Yo
+Wǒ
+我
+
+Hablar
+shuō
+说
+
+Yo hablo Español
+wǒ shuō xī bān yá yǔ
+我说西班牙语
+
+Inglés
+yīng yǔ
+英语
+
+Chino
+hàn yǔ
+汉语
