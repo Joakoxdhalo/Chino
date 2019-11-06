74093a6f452c011f6c2926b19c542eab95d2a8cc
Joakoxdhalo <joacojimeca@gmail.com>
2019-11-06 14:27:51 -0500

    * Add Repaso_Antes_Parcial2Chino1.py

---
+++ Clases/Repaso_Antes_Parcial2Chino1.py
@@ -0,0 +1,126 @@
+1) Me gusta ver la película
+wǒ xǐ huān kàn diàn yǐng ---> Ojo que xǐ huān es con tono 3.
+我喜欢看电影
+
+2) Dónde está el restaurante chino?
+zhōng guó cān tīng zài nǎ?
+中国餐厅在哪?
+
+3) De dónde es tú profesor de inglés?
+nǐ de yīng yǔ lǎo shī shì nǎ guó rén?
+你的英语老师是哪国人?
+
+4) Hoy es 2019 año, 10 mes, 28 día, lunes
+jīn tiān shì 2019 nián, 10 yuè 28 hào, xīng qī yī
+今天是 2019 年， 10 月， 28 号， 星期一
+
+5) A los chinos les gusta jugar ping pong, a los colombianos les gusta jugar fútbol
+zhōng guó rén xī huān dǎ pīng pāng qiú, gē lún bǐ yà rén xī huān tī zú qiú
+中国人喜欢打乒乓球，哥伦比亚人喜欢踢足球
+
+B)
+1) De dónde eres tú?
+nǐ shì nǎ guó rén?
+你是哪国人
+Soy colombiano
+wǒ shì gē lún bǐ yà rén
+我是哥伦比亚人
+
+2) Cómo se llama usted?
+nǐ jiào shén me?
+你叫什么?
+Me llamo Joaquín
+wǒ jiào Joaquín
+我叫Joaquín
+
+3) Qué te gusta comer?
+nǐ xǐ huān chī zài me?
+你喜欢吃在么?
+
+Me gusta comer comida francesa
+wǒ xǐ huān chī fà guó cài
+我喜欢吃法国菜
+
+4) Te gusta ver películas chinas?
+nǐ xǐ huān kàn zhōng guó diàn yǐng ma?
+你喜欢看中国电影吗?
+
+Si, me gusta ver películas chinas
+shì, wǒ xǐ huān kàn diàn yǐng
+是， 我喜欢看电影
+
+5) Qué idioma hablas?
+nǐ shuō shén me yǔ?
+你说什么与?
+
+Yo hablo chino, español e inglés
+wǒ shuō hàn yǔ, xī bān yá yǔ hé yīng yǔ
+我说汉语， 西班牙语和英语
+
+
+6) Tomas té?
+nǐ hē chà ma?
+你喝茶吗?
+No
+bù
+不
+
+7) Te gusta jugar fútbol?
+nǐ xǐ huān tī zú qiú ma?
+你喜欢踢足球吗?
+
+Si
+shì
+是
+
+8) Te gusta jugar ping pong?
+nǐ xǐ huān dǎ pīng pāng qiú?
+你喜欢打乒乓球吗?
+
+9) Te gusta comer comida china?
+nǐ chī zhōng guó cài ma?
+你吃中国菜吗?
+
+10) Tú inglés muy bien?
+nǐ de yīng yǔ hěn hǎo ma?
+你的英语很好吗?
+
+
+C)
+银行   yíng háng
+商店   shāng diàn
+超市   chāo shì
+餐厅   cān tīng
+厕所   cè suǒ
+飞机   fēi jī
+自行车 zì xíng chē
+汽车   qì chē
+地铁   dì tiě
+
+
+D)
+1) 篮球   lán qiú
+2) 排球   pái qiú
+3) 乒乓球 pīng pāng qiú
+4) 网球   wǎng qiú
+5) 足球   zú qiú
+
+
+Escriba símbolos:
+
+nǐ hǎo
+你好
+
+zài jiàn
+再见
+
+xiè xiè
+谢谢
+
+duì bù qǐ
+对不起
+
+gē lún bǐ yà
+哥伦比亚
+
+dferasor
